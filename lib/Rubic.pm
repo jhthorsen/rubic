9879635b4f1d65eff0953c59cdc03da006658e0a
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-08-12 16:20:10 +0200

    Released version 0.0804
           * Compatible with Mojolicious 5

--- lib/Rubic.pm
+++ lib/Rubic.pm
@@ -6,7 +6,7 @@ Rubic - Remote admin tool for ubic
 
 =head1 VERSION
 
-0.0803
+0.0804
 
 =head1 DESCRIPTION
 
@@ -72,7 +72,7 @@ argument.
 
 use strict;
 
-our $VERSION = '0.0803';
+our $VERSION = '0.0804';
 
 =head1 COPYRIGHT
 
